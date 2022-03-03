6945e34c8aa06ebdd0b034074903fd90c50c6900
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-03-04 03:00:42 +0600

    practise settime out function

---
+++ timeout.js
@@ -0,0 +1,16 @@
+function doSomething() {
+    console.log('i am coding javasciprt');
+}
+console.log("first: ami shobar age");
+console.log("Second: i am the 2nd perosn");
+
+// setTimeout(doSomething, 1000);
+setTimeout(function () {
+    console.log("i am using vs code");
+}, 2000);
+
+setTimeout(()=>{
+    console.log('exploring mdn article');
+},1000)
+console.log("Third: i am the 3rd perosn");
+console.log("Fourth: i am the 4th perosn");
